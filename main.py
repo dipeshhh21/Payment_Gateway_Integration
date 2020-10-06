9b154ae0fcf13f873086b624eaa50fc84af61320
Dipesh Kamble <68158873+dipeshhh21@users.noreply.github.com>
2020-10-06 22:28:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request,redirect
+from instamojo_wrapper import Instamojo
+
+API_KEY = "test_34d92c4b1e30ddbbd4f5c93c6ad"
+AUTH_TOKEN = "test_5fda6a8f4ab529c28819165444d"
+
+api = Instamojo(api_key=API_KEY,auth_token=AUTH_TOKEN,endpoint='https://test.instamojo.com/api/1.1/')
+
+app = Flask(__name__)
+@app.route('/',methods = ['GET', 'POST'])
+def home():
+    return render_template('index.html')
+
+@app.route('/form')
+def form():
+   return render_template('form.html')
+
+@app.route('/success')
+def success():
+    return render_template('success.html',)
+    
+@app.route('/pay',methods=['POST','GET'])
+def pay():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        purpose = request.form.get('purpose')
+        email = request.form.get('email')
+        amount = request.form.get('amount')
+        
+        response = api.payment_request_create(
+        amount=amount,
+        purpose=purpose,
+        buyer_name=name,
+        send_email=True,
+        email=email,
+        redirect_url="http://localhost:5000/success"
+        )
+        
+        return redirect(response['payment_request']['longurl'])
+    
+    else:
+        
+        return redirect('/')
+
+    
+if __name__ == '__main__':
+   app.run(debug=True)
